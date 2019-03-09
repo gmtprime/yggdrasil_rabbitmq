5020a2832a9a35837e87bf1007416114ae94a06e
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2019-03-09 13:37:52 +0100

    Added tests for channel cache

--- test/yggdrasil/rabbitmq/connection/generator_test.exs
+++ test/yggdrasil/rabbitmq/connection/generator_test.exs
@@ -2,39 +2,42 @@ defmodule Yggdrasil.RabbitMQ.Connection.GeneratorTest do
   use ExUnit.Case
 
   alias AMQP.Channel
+  alias Yggdrasil.RabbitMQ.Client
   alias Yggdrasil.RabbitMQ.Connection.Generator
 
-  describe "with_channel/2" do
-    test "returns a new channel" do
-      tag = :publisher
+  setup do
+    client = %Client{pid: self(), tag: :publisher, namespace: __MODULE__}
+    {:ok, [client: client]}
+  end
 
-      assert {:ok, %Channel{pid: pid}} = Generator.with_channel(tag, nil)
+  describe "with_channel/2" do
+    test "returns a new channel", %{client: client} do
+      assert {:ok, %Channel{pid: pid}} = Generator.with_channel(client)
       assert is_pid(pid) and Process.alive?(pid)
     end
 
-    test "returns the same channel for the same process" do
-      tag = :publisher
-
-      assert {:ok, %Channel{pid: pid}} = Generator.with_channel(tag, nil)
-      assert {:ok, %Channel{pid: ^pid}} = Generator.with_channel(tag, nil)
+    test "returns the same channel for the same process", %{client: client} do
+      assert {:ok, channel} = Generator.with_channel(client)
+      assert {:ok, ^channel} = Generator.with_channel(client)
     end
 
-    test "returns an error when no connection is available" do
-      tag = :publisher
-      namespace = Disconnected
+    test "returns an error when no connection is available",
+           %{client: client} do
+      namespace = __MODULE__.Disconnected
       config = [rabbitmq: [hostname: "disconnected"]]
       Application.put_env(:yggdrasil, namespace, config)
 
-      assert {:error, _} = Generator.with_channel(tag, namespace)
+      client = %Client{client | namespace: namespace}
+
+      assert {:error, _} = Generator.with_channel(client)
     end
   end
 
   describe "with_channel/3" do
-    test "returns a custom callback" do
-      tag = :publisher
+    test "returns a custom callback", %{client: client} do
       callback = fn _channel -> :ok end
 
-      assert :ok = Generator.with_channel(tag, nil, callback)
+      assert :ok = Generator.with_channel(client, callback)
     end
   end
 end
